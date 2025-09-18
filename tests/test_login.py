b3aff96fe3e6cff184be9493867a03107d7febbe
liamnoel007 <oasisamb@gmail.com>
2025-09-18 16:58:30 +0300

    1

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,14 +9,4 @@ def test_incorrect_login_and_password(login_page):
         login_page.fill_login_form("123@123.com", "123123")
 
     with allure.step("Проверить сообщение об ошибке"):
-        try:
-            login_page.check_error_text("Authentication failed.")
-        except AssertionError:
-            # Делаем скриншот и прикладываем в отчёт
-            screenshot = login_page.page.screenshot()
-            allure.attach(
-                screenshot,
-                name="Ошибка авторизации",
-                attachment_type=allure.attachment_type.PNG,
-            )
-            raise
+        login_page.check_error_text("Authentication failed.")
