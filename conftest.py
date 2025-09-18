c6330bb0b3b2e037b8a2333207f7b24bfac72702
liamnoel007 <oasisamb@gmail.com>
2025-09-18 16:57:08 +0300

    1

--- conftest.py
+++ conftest.py
@@ -12,3 +12,31 @@ def login_page(page):
 @pytest.fixture()
 def tshirt_page(page):
     return TshirtsPage(page)
+
+
+import pytest
+import allure
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+
+        page_obj = None
+        for arg in item.funcargs.values():
+
+            if hasattr(arg, "page"):
+                page_obj = arg
+                break
+
+        if page_obj:
+            screenshot = page_obj.page.screenshot()
+            allure.attach(
+                screenshot,
+                name=f"Скриншот-{item.name}",
+                attachment_type=allure.attachment_type.PNG,
+            )
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,22 @@
 import allure
 
 
-@allure.feature("Авторизация")
-class TestLogin:
+def test_incorrect_login_and_password(self, login_page):
+    with allure.step("Открыть страницу логина"):
+        login_page.open_page()
 
-    @allure.story("Неверный логин и пароль")
-    @allure.severity(allure.severity_level.CRITICAL)
-    def test_incorrect_login_and_password(self, login_page):
-        with allure.step("Открыть страницу логина"):
-            login_page.open_page()
+    with allure.step("Ввести неверные логин и пароль"):
+        login_page.fill_login_form("123@123.com", "123123")
 
-        with allure.step("Ввести неверные логин и пароль"):
-            login_page.fill_login_form("123@123.com", "123123")
-
-        with allure.step("Проверить сообщение об ошибке"):
-            try:
-                login_page.check_error_text("Authentication failed.")
-            except AssertionError:
-                # Делаем скриншот и прикладываем в отчёт
-                screenshot = login_page.page.screenshot()
-                allure.attach(
-                    screenshot,
-                    name="Ошибка авторизации",
-                    attachment_type=allure.attachment_type.PNG,
-                )
-                raise
+    with allure.step("Проверить сообщение об ошибке"):
+        try:
+            login_page.check_error_text("Authentication failed.")
+        except AssertionError:
+            # Делаем скриншот и прикладываем в отчёт
+            screenshot = login_page.page.screenshot()
+            allure.attach(
+                screenshot,
+                name="Ошибка авторизации",
+                attachment_type=allure.attachment_type.PNG,
+            )
+            raise
