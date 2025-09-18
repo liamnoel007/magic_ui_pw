cc7087fbd32410996dcc96e506d653968803ff03
liamnoel007 <oasisamb@gmail.com>
2025-09-18 16:26:56 +0300

    1

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,7 @@
 def test_incorrect_login_and_password(login_page):
+    @allure.step("Открыть страницу логина")
     login_page.open_page()
+    @allure.step("Ввод неверный логина и пароля")
     login_page.fill_login_form("123@123.com", "123123")
+    @allure.step("Открыть страницу логина")
     login_page.check_error_text("Authentication failed.")
